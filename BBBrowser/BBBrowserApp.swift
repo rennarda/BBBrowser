0887b573585e8818629e6e1516e765dd0d10a1cb
Andy Rennard <rennard@mac.com>
2021-04-23 15:41:47 +0100

    Initial Commit

---
+++ BBBrowser/BBBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBBrowserApp.swift
+//  BBBrowser
+//
+//  Created by AndyRennard on 23/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct BBBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
