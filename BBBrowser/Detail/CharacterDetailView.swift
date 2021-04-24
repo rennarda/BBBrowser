f311e21abbce1f416b5a813edee47a853f59c1c2
Andy Rennard <rennard@mac.com>
2021-04-24 22:42:36 +0100

    Fills out character detail view

--- BBBrowser/Detail/CharacterDetailView.swift
+++ BBBrowser/Detail/CharacterDetailView.swift
@@ -11,7 +11,40 @@ struct CharacterDetailView: View {
     let character: BreakingBadCharacter
 
     var body: some View {
-        Text(character.name)
+        VStack(spacing: 10) {
+            Text(character.name)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+
+            Text(character.nickname)
+                .font(.title)
+                .italic()
+                .foregroundColor(.secondary)
+
+            URLImage(url: character.imageURL)
+                .aspectRatio(contentMode: .fill)
+                .border(Color.white, width: 5)
+                .shadow(color: .black.opacity(0.5), radius: 4, x: 0, y: 3)
+                .rotationEffect(Angle(degrees: Double.random(in: -5...5)))
+                .padding(50)
+
+            Text(character.occupation.joined(separator: ",\n"))
+                .font(.subheadline)
+
+            HStack {
+                Text("Status:")
+                Text(character.status).fontWeight(.bold)
+            }
+
+            Text("Seasons: " + character.episodeAppearances
+                    .map{"\($0)"}
+                    .joined(separator: ", "))
+
+            Spacer()
+        }
+        .padding()
+        .multilineTextAlignment(.center)
+
     }
 
     init(_ character: BreakingBadCharacter) {
